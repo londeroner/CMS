2364fc2c643c4e9c5ce8aeeecfb37e2e69f90022
londeroner <Filin_s1999@mail.ru>
2022-03-22 14:58:00 +0300

    Added side and top bars; font-awesome installed

---
+++ CMS/wwwroot/lib/custom/js/admin/admin.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    $(initSidebar);
+
+    function initSidebar() {
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    }
+})();
