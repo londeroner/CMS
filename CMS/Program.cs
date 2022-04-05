b5264fd8b6fa6bba55f2566376892fb5014f7e1e
londeroner <Filin_s1999@mail.ru>
2022-04-05 14:28:11 +0300

    using clear

--- CMS/Program.cs
+++ CMS/Program.cs
@@ -1,19 +1,14 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Serilog;
-using System.Diagnostics;
-using LoggingService;
-using System.IO;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog.Sinks.SystemConsole.Themes;
 using DataService;
 using FunctionalService;
+using LoggingService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace CMS
 {
--- FunctionalService/FunctionalSvc.cs
+++ FunctionalService/FunctionalSvc.cs
@@ -4,8 +4,6 @@ using ModelService;
 using Serilog;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FunctionalService
